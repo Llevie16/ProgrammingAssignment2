c80a5963fb3c05c9ee1b8a4c8117d45b1b095741
Llevie16 <lbgonzales@mymail.mapua.edu.ph>
2021-01-20 21:31:23 +0800

    Create makeCacheMatrix.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,33 @@
+library(MASS)
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function()x
+  setinv <- function(inverse)inv <<- inverse
+  getinv <- function() {
+    inv<-ginv(x)
+    inv%*%x
+  }
+  list(set = set,
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+cacheSolve <- function(x, ...) 
+{
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data!")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinv(inv)
+  inv
+}
